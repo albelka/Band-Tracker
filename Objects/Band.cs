18044f60a2391ca688d2d198f03ff4d5dd05744e
Anne Belka <antastic461@gmail.com>
2016-12-16 09:14:22 -0800

    add getall and save tests for Band

---
+++ Objects/Band.cs
@@ -0,0 +1,108 @@
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class Band
+  {
+
+    private int _id;
+    private string _name;
+
+
+    public Band(string Name, int Id = 0)
+    {
+      _name = Name;
+      _id = Id;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public override bool Equals(System.Object otherBand)
+    {
+      if (!(otherBand is Band))
+      {
+        return false;
+      }
+      else
+      {
+        Band newBand = (Band) otherBand;
+        bool nameEquality = (this.GetName() == newBand.GetName());
+        return (nameEquality);
+      }
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO bands (name) OUTPUT INSERTED.id VALUES (@BandName);", conn);
+
+      SqlParameter bandNameParameter = new SqlParameter("@BandName", this.GetName());
+      cmd.Parameters.Add(bandNameParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int BandId = rdr.GetInt32(0);
+        string BandName = rdr.GetString(1);
+        Band newBand = new Band(BandName, BandId);
+        allBands.Add(newBand);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      return allBands;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM bands;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
---
+++ Tests/BandTest.cs
@@ -0,0 +1,60 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class BandTest : IDisposable
+  {
+    public BandTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Equals_ReplacesEqualObjects()
+    {
+
+      Band bandOne = new Band("Wilco");
+      Band bandTwo = new Band("Wilco");
+
+      Assert.Equal(bandOne, bandTwo);
+    }
+
+    [Fact]
+    public void GetAll_RetrievesAllBandsFromDatabase()
+    {
+      //Arrange
+      Band bandOne = new Band("Wilco");
+      bandOne.Save();
+      Band bandTwo = new Band("John Prine");
+      bandTwo.Save();
+      // Act
+      int result = Band.GetAll().Count;
+
+      //Assert
+      Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Save_SavesBandToDatabase()
+    {
+      //Arrange
+      Band testBand = new Band("Wilco");
+      testBand.Save();
+      //Act
+
+      List<Band> result = Band.GetAll();
+      List<Band> testList = new List<Band>{testBand};
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    public void Dispose()
+    {
+      Band.DeleteAll();
+    }
+  }
+}
